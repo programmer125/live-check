b3f97913ee8235cdf405c501c19db63fc71efdd7
duyuxuan <duyuxuan@yixia.com>
2025-08-20 11:29:17 +0800

    db pool

--- db/session.py
+++ db/session.py
@@ -3,8 +3,35 @@
 # @Time : 2024/10/22 17:39
 # @File : session.py
 import redis
+from sqlalchemy import create_engine
+from sqlalchemy.pool import QueuePool
+from sqlalchemy.orm import sessionmaker
 
 from configs.settings import settings
+
+
+playlist_engine = create_engine(
+    settings.playlist_db_uri,
+    echo=False,
+    pool_size=2,
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    poolclass=QueuePool,
+)
+PlaylistSessionLocal = sessionmaker(bind=playlist_engine)
+
+
+neoailive_engine = create_engine(
+    settings.neoailive_db_uri,
+    echo=False,
+    pool_size=2,
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    poolclass=QueuePool,
+)
+NeoailiveSessionLocal = sessionmaker(bind=neoailive_engine)
 
 
 log_pool = redis.ConnectionPool.from_url(
