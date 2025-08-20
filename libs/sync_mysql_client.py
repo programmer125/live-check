5dd49d15b318ac947d2fe2dbfc7d2f53ed35dcb4
duyuxuan <duyuxuan@yixia.com>
2025-08-20 11:34:38 +0800

    修改查询

--- libs/sync_mysql_client.py
+++ libs/sync_mysql_client.py
@@ -6,30 +6,35 @@
 # @Author : duyuxuan
 # @Time : 2025/2/28 17:25
 # @File : mysql_client.py
-from sqlalchemy import create_engine, text
-
-from configs.settings import settings
+from sqlalchemy import text
 
 
 class MysqlClient(object):
-    def __init__(self, db_url):
+    def __init__(self, db_session):
         # 创建连接
-        self.engine = create_engine(db_url)
+        self.db_session = db_session
 
     def fetch_one(self, sql):
-        with self.engine.connect() as conn:
-            response = conn.execute(text(sql))
+        with self.db_session.begin():
+            response = self.db_session.execute(text(sql))
             results = response.mappings().all()
             return results[0]
 
     def fetch_all(self, sql):
-        with self.engine.connect() as conn:
-            response = conn.execute(text(sql))
+        with self.db_session.begin():
+            response = self.db_session.execute(text(sql))
             results = response.mappings().all()
             return results
 
     def fetch_count(self, sql):
-        with self.engine.connect() as conn:
-            response = conn.execute(text(sql))
+        with self.db_session.begin():
+            response = self.db_session.execute(text(sql))
             results = response.mappings().all()
             return results[0]["cnt"]
+
+
+if __name__ == "__main__":
+    from db.session import PlaylistSessionLocal
+
+    instance = MysqlClient(PlaylistSessionLocal())
+    print(instance.fetch_count("select count(*) as cnt from rt_room limit 10"))
